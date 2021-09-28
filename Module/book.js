18e3121d21394b041d32d62476a3446a43060747
ammarzeyad <ammarramadan16@yahoo.com>
2021-09-28 23:04:07 +0300

    error

--- Module/book.js
+++ Module/book.js
@@ -50,12 +50,32 @@ function seedBookInformation() {
     book5.save();
 }
 
-// seedBookInformation();
+seedBookInformation();
 
 // http://localhost:3001/books?emailin=
+async function addBookHandler(req,res){
+    let { title, description, status, email } = req.body;
+    await BooksModel.create({
+        title,
+        description,
+        status,
+        email
+    })
+
+    BooksModel.find({},(error,data) => {
+        if(error) {
+            console.log('error in getting data',error);
+        } else {
+            res.send(data);
+        }
+    })
+}
+
+
+// http://localhost:3001/books
 function getBooksHandler(req,res){
-    let emailin = req.query.emailin
-    BooksModel.find({email:emailin},function(error,Data) {
+    // let emailin = req.query.emailin
+    BooksModel.find({},(error,Data)=> {
         if(error) {
             console.log('error in getting data',error)
         } else {
@@ -64,5 +84,19 @@ function getBooksHandler(req,res){
     })
 }
 
+function deleteBookHandler(req, res) {
+    let {bookID} = req.query;
 
-module.exports = getBooksHandler;
+    BooksModel.deleteOne({ _id: bookID }).then(() => {
+        BooksModel.find({}, function (error, data) {
+            if (error) {
+                console.log('error in getting data', error)
+            } else {
+                res.send(data)
+            }
+        })
+    })
+}
+
+
+module.exports = {getBooksHandler , addBookHandler , deleteBookHandler};
